bfe1350aeba41b2f766087e69b557190b95db791
Daniel Okwufulueze <daniel@dealdey.com>
2015-09-21 10:40:07 +0100

    Program from command line.

--- Exercise1/bin/main.rb
+++ Exercise1/bin/main.rb
@@ -1,10 +1,10 @@
-require_relative "../lib/method_creator_caller.rb"
+require_relative "../lib/method_creator.rb"
 
 puts "Enter the name of your method"
 method = gets.chomp
 puts "Enter a one-line code of your method"
 code = gets.chomp
-creator = MethodCreatorCaller.new
+creator = MethodCreator.new
 creator.instance_eval("def #{method}; #{code}; end")
 creator.send(method)
 
---
+++ Exercise1/lib/method_creator.rb
@@ -0,0 +1,3 @@
+class MethodCreator
+end
+
